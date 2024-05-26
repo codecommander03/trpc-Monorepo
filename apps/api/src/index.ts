61720320b1a749d6a4005854467b1df7e5f9cff6
Aditya <codecommander03@gmail.com>
2024-05-26 22:01:22 +0530

    formatted by prettier

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send("Hello World")
-});
+  res.send('Hello World')
+})
 
 app.listen(8080, () => console.log(`Listening on port 8080`))
